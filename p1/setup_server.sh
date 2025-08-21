8628e3a3391b6424fd5789b1632cf8fa9d71e018
Romina <romina.aghbolaghi@gmail.com>
2025-08-21 18:13:24 +0200

    p1
    last version

--- p1/setup_server.sh
+++ p1/setup_server.sh
@@ -1,4 +1,12 @@
 #!/bin/bash
+set -euo pipefail
+
+# Install K3S Server
+# Kubectl (auto installed by k3s install script): https://docs.k3s.io/quick-start#install-script
+# K3S Env vars: https://docs.k3s.io/reference/env-variables
+# K3S Server: https://docs.k3s.io/cli/server
+# K3S Config: https://docs.k3s.io/installation/configuration#configuration-file
+
 SERVER_HOST="${K3S_SERVER_HOST:-192.168.56.110}"
 SERVER_PORT="${K3S_SERVER_PORT:-6443}"
 
@@ -49,44 +57,3 @@ done
 
 # Save the token for agents to use
 cat /var/lib/rancher/k3s/server/token > /vagrant/k3s.token
-
-# set -euo pipefail
-
-# Install K3S Server
-# Kubectl (auto installed by k3s install script): https://docs.k3s.io/quick-start#install-script
-# K3S Env vars: https://docs.k3s.io/reference/env-variables
-# K3S Server: https://docs.k3s.io/cli/server
-# K3S Config: https://docs.k3s.io/installation/configuration#configuration-file
-
-# Create a directory for the K3S configuration
-# mkdir -p /etc/rancher/k3s
-# cp /vagrant/k3s-server-config.yaml /etc/rancher/k3s/config.yaml
-
-# Install and run the server
-# curl -sfL https://get.k3s.io | INSTALL_K3S_EXEC="server" sh - 
-
-# echo "Waiting for server node to become Ready..."
-# for i in {1..60}; do
-#   if k3s kubectl get nodes 2>/dev/null | grep -q 'Ready'; then
-#     break
-#   fi
-#   sleep 2
-# done
-#
-# Point kubectl to the k3s kubeconfig (optional if using 'k3s kubectl')
-# export KUBECONFIG=/etc/rancher/k3s/k3s.yaml
-
-# echo "Check for K3S Server ready"
-# timeout=60
-# waited=0
-# while ! k3s kubectl get nodes --no-headers 2>/dev/null | grep -q 'Ready' && [ $waited -lt $timeout ]; do
-#   echo -ne "\rWaited for k3s server... ($waited/$timeout)"
-#   sleep 5
-#   waited=$((waited+5))
-# done
-#
-# if [ $waited -ge $timeout ]; then
-#   echo "Server couldnt be reached after $waited s"
-#   exit 1
-# fi
-
