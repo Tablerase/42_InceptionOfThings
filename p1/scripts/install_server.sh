e935d8a75ef7623b9bcdac4ab94482b931dbb1a2
Romina <romina.aghbolaghi@gmail.com>
2025-08-22 12:05:51 +0200

    p1
    all version of installing k3s with env with flags and without them, and all methods work well

--- p1/scripts/install_server.sh
+++ p1/scripts/install_server.sh
@@ -25,21 +25,22 @@ fi
 # When you install K3s without K3S_URL and K3S_TOKEN,The node becomes a server node (master / control plane).
 # Install K3s server (this will be our control plane node)
 # It runs the control plane components: API server, scheduler, controller manager, etc.
-curl -sfL https://get.k3s.io | INSTALL_K3S_EXEC="server \
-  --node-ip=192.168.56.110 \
-  --bind-address=192.168.56.110 \
-  --advertise-address=192.168.56.110" sh -
 
-# # Make sure kubectl is set up for the vagrant user
-# sudo mkdir -p /home/vagrant/.kube
-# sudo cp /etc/rancher/k3s/k3s.yaml /home/vagrant/.kube/config
-# sudo chown -R vagrant:vagrant /home/vagrant/.kube/config
+curl -sfL https://get.k3s.io | sh -
 
+##### istallation with environment variables, and command flags
+# export NODE_IP=192.168.56.110
+# export INSTALL_K3S_EXEC="server \
+#   --node-ip=$NODE_IP \
+#   --bind-address=$NODE_IP \
+#   --advertise-address=$NODE_IP"
+# curl -sfL https://get.k3s.io | sh -
 
-# # Get the token for the worker nodes
-# TOKEN=$(sudo cat /var/lib/rancher/k3s/server/node-token)
-# # Store the token for the workers to use
-# echo $TOKEN > /vagrant/confs/node-token
+##### istallation with command flags
+# curl -sfL https://get.k3s.io | INSTALL_K3S_EXEC="server \
+#   --node-ip=192.168.56.110 \
+#   --bind-address=192.168.56.110 \
+#   --advertise-address=192.168.56.110" sh -
 
 # Ensure the shared folder exists
 sudo mkdir -p /vagrant/.confs
--- p1/scripts/install_worker.sh
+++ p1/scripts/install_worker.sh
@@ -8,100 +8,21 @@ sudo apt-get upgrade -y
 # Install dependencies
 sudo apt-get install -y curl
 
-# Server IP
-SERVER_IP="192.168.56.110"
+#### istallation with environment variables
+# curl -sfL https://get.k3s.io | \
+#   INSTALL_K3S_EXEC="agent" \
+#   K3S_URL="https://192.168.56.110:6443" \
+#   K3S_TOKEN="$(cat /vagrant/.confs/node-token)" \
+#   K3S_NODE_IP="192.168.56.111" \
+#   sh -
 
-# sudo systemctl stop k3s-agent
-# sudo rm -rf /etc/rancher/k3s /var/lib/rancher/k3s
+export NODE_IP=192.168.56.111
+export SERVER_IP=192.168.56.110
 
-curl -sfL https://get.k3s.io | \
-  INSTALL_K3S_EXEC="agent" \
-  K3S_URL="https://192.168.56.110:6443" \
-  K3S_TOKEN="$(cat /vagrant/.confs/node-token)" \
-  K3S_NODE_IP="192.168.56.111" \
-  sh -
-# Make kubectl available for vagrant user (optional)
-# sudo mkdir -p /home/vagrant/.kube
-# sudo cp /etc/rancher/k3s/k3s.yaml /home/vagrant/.kube/config || true
-# sudo chown -R vagrant:vagrant /home/vagrant/.kube/config
+export K3S_URL="https://$SERVER_IP:6443"
+export K3S_TOKEN=$(cat /vagrant/.confs/node-token)
+export INSTALL_K3S_EXEC="agent --node-ip=$NODE_IP"
+
+curl -sfL https://get.k3s.io | sh -
 
 echo "✅ K3s worker installation completed and should join the cluster."
-
-
-
-
-
-######################################################################################################################################
-######################################################################################################################################
-
-
-
-# #!/bin/bash
-# set -e
-
-# # ------------------------
-# # Update system and install dependencies
-# # ------------------------
-# sudo apt-get update -y
-# sudo apt-get upgrade -y
-# sudo apt-get install -y curl
-
-# # ------------------------
-# # Disable firewall (ufw)
-# # ------------------------
-# if sudo ufw status | grep -q "active"; then
-#     sudo ufw disable
-#     echo "✅ Firewall disabled"
-# else
-#     echo "✅ Firewall already inactive"
-# fi
-
-# # ------------------------
-# # Wait for node-token from server
-# # ------------------------
-# while [ ! -f /vagrant/confs/node-token ]; do
-#     echo "⏳ Waiting for node-token from server..."
-#     sleep 2
-# done
-
-# echo $(cat /vagrant/confs/node-token)
-# sudo cp /vagrant/confs/node-token /tmp/node-token
-
-# TOKEN=$(cat /tmp/node-token | tr -d '\n')
-# # TOKEN=$(cat /vagrant/confs/node-token)
-# SERVER_IP=$1
-
-# # ------------------------
-# # Clean previous K3s agent installation
-# # ------------------------
-# # if [ -f /usr/local/bin/k3s ]; then
-# #     echo "🧹 Removing previous K3s installation..."
-# #     sudo /usr/local/bin/k3s-agent-uninstall.sh || true
-# # fi
-
-# # sudo rm -rf /var/lib/rancher/k3s /etc/rancher/k3s
-# # sudo rm -f /etc/systemd/system/k3s-agent.service
-# # sudo systemctl daemon-reload
-
-# # ------------------------
-# # Install K3s agent
-# # ------------------------
-# echo "⚡ Installing K3s agent..."
-# curl -sfL https://get.k3s.io | \
-# K3S_URL="https://$SERVER_IP:6443" \
-# K3S_TOKEN="$TOKEN" \
-# sh -
-
-
-# # ------------------------
-# # Reload systemd and restart agent
-# # ------------------------
-# # sudo systemctl daemon-reexec
-# # sudo systemctl restart k3s-agent
-
-# echo "✅ K3s worker installation completed. Node should now be connected to the cluster."
-
-# # ------------------------
-# # Tail logs
-# # ------------------------
-# # sudo journalctl -u k3s-agent -f --no-pager
