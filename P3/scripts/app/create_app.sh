3c094380d7eb6a839c5b53986babf3cd5267628c
romina <aghbolaghi.romina@gmail.com>
2025-08-30 14:33:27 +0200

    the version with rollout and sync none for argocd

--- P3/scripts/app/create_app.sh
+++ P3/scripts/app/create_app.sh
@@ -3,22 +3,28 @@ set -euo pipefail
 
 ARGOCD_SERVER=localhost:8080
 ARGOCD_USER=admin
-ARGOCD_PASS=hqiOholprq5NrGeQ
+ARGOCD_PASS=Pkef3KZ-T3PLUrpr
 # ARGOCD_PASS=$(kubectl -n argocd get secret argocd-initial-admin-secret -o jsonpath="{.data.password}" | base64 -d)
 
 # Login
 argocd login $ARGOCD_SERVER --username $ARGOCD_USER --password $ARGOCD_PASS --insecure
 
 
-# Create app
+# Create app on argocd and send request to argocd api server 
+# with --sync-policy none we can rollout to the previous version of replicaset
+# address of api-server in kubernetes https://kubernetes.default.svc <service name>.<namespace>.svc
 argocd app create wil-playground \
   --repo https://github.com/Romina-M-A/rmohamma_42_iot_app \
   --path . \
   --dest-server https://kubernetes.default.svc \
   --dest-namespace dev \
-  --sync-policy automated \
-  --auto-prune \
-  --self-heal
+  --sync-policy none
 
-# # force the first sync immediately
+  ## with --sync-policy automated we can not rollout to previous replicaset
+  # --sync-policy automated \
+  # --auto-prune \
+  # --self-heal
+
+
+# # create app on dev env send request to kubernetes api server
 argocd app sync wil-playground
